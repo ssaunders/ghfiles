98a409870ff78ccb8ca136bd049f439b5b32b5f0
ssaunders <stersaun@gmail.com>
2023-11-09 22:11:42 -0700

    Update bo_customization.js
    Changed a few things I don't remember. Updated Library. Added two spaces between fn sections. Added unloadBO(). Started update for new shortcut info pull.

--- bo_customization.js
+++ bo_customization.js
@@ -1,5 +1,7 @@
 // TODO: 
 
+// TODO: Add the name of T3 AOR, for non-standard RFI's
+// TODO: Add the effective date, for non-standard RFI's
 // TODO: Make it so that an Alt Addr is created, attached to the bottom of the copy fn. City, ST Zip
 // TODO: Pipe dream--create page that wraps BO in an iframe, so I can keep my shortcuts/apply them auto on refresh
 	//TODO: Make it so the bol search calls that page
@@ -16,90 +18,160 @@
 		// TODO: shortcut to copy sub date
 		// TODO: shortcut to copy plan name
 		// TODO: Figure out if can return just plain text.
+	// TODO: Make unloadBO fn
 
 
-/*  Function DEFAULT
+
+/* Function DEFAULT
 	NOTES_ON_FN */
 
 /*************
  * FUNCTIONS
  *************/
 
+/*** LIBRARY ***/
+
+	/* Function alreadyPresent
+		alerts that the code already exists */
+	function alreadyPresent() {
+		console.warn(">> MARx Code already present");
+	}
+
+	/* Function DEBUG FUNCTIONS
+		tests for/starts/stops debug */
+	function isDB() {
+		return document.mxdebug;
+	}
+	function startDB() {
+		document.mxdebug = true;
+	}
+	function endDB() {
+		document.mxdebug = false;	
+	}
+
+	/* Function copyStringToClipboard
+		Copies a string to the computer clipboard */
+	function copyStringToClipboard(string) {
+		if(string == null) {
+			console.warn("Nothing to copy");
+			return;
+		}
+
+		navigator.clipboard.writeText(string).then(() => {
+		  console.log('Content copied to clipboard');
+		},() => {
+		  console.error('Failed to copy');
+		});
+	}
+
+	/* Function copyElToClipboard
+		Copies the content of an el to the computer clipboard */
+	function copyElToClipboard(htmlEl) {
+		if(htmlEl == null) {
+			console.warn("Nothing to copy");
+			return;
+		}
+
+	    var range = document.createRange();
+	    var sel = document.getSelection();
+
+	    sel.removeAllRanges();
+	    range.selectNodeContents(htmlEl);
+		sel.addRange(range);
+		document.execCommand("Copy");
+	}
+
+	/*  Function addCssEl
+		Adds the passed in CSS text to the document body */
+	function addCssEl(cssText, doc) {
+		doc = (doc == null || doc == undefined) ? document : doc;
+
+		if (cssText!=null) {
+			const css_el = doc.createElement("style");
+			css_el.textContent = cssText;
+			doc.childNodes[1].appendChild(css_el);
+		}
+	}
+
+	/*  Function addJsScript
+		Adds the passed in script text to the document body */
+	function addJsScript(scriptText, doc) {
+		doc = (doc == null || doc == undefined) ? document : doc;
+
+		if (scriptText!=null) {
+			const js_el = doc.createElement("script");
+			js_el.textContent = scriptText;
+			doc.childNodes[1].appendChild(js_el);
+		}
+	}
+
+	/* Function getCurrentTimestamp
+		Returns a string of the current timestamp */
+	function getCurrentTimestamp() {
+		return new Date().toLocaleString('en-us',{hour:'numeric',minute:'numeric',second:'numeric'});
+	}
+
+
 /*** UTILITY ***/
 
-/*  Function alreadyPresent
-	alerts that the code already exists */
-function alreadyPresent() {
-	console.warn("BO Code already present");
-}
-
-/*  Function addScript
-	Adds the passed in script text and CSS text to the document body */
-function addScript(scriptText, cssText) {
-	/*Add style element*/
-	if (cssText!=null) {
-		const css_el = document.createElement("style");
-		css_el.textContent = cssText;
-		document.childNodes[1].appendChild(css_el);
-	}
-
-	/*Add js element*/
-	if (scriptText!=null) {
-		const js_el = document.createElement("script");
-		js_el.textContent = scriptText;
-		document.childNodes[1].appendChild(js_el);
-	}
-}
-
-/*  Function setUpKeyboardShortcuts
+/* Function setUpKeyboardShortcuts
 	Sets up the keyboard listeners to the page */
 function setUpKeyboardShortcuts() {
 	document.addEventListener("keyup", selectAppInfo);
-	// document.addEventListener("keyup", selectFirstInfo);
 	console.log("set up shortcuts");
 }
 
+/* Function unloadBO
+	Sets up the keyboard listeners to the page */
+function unloadBO() {
+	document.removeEventListener("keyup", selectAppInfo);
+	console.log("removed shortcuts");
+	document.ranSetup = false;
+}
+
+
 /*** TOOLTIP SETUP ***/
-// ??? Should I just make the note el change on hover?
+	// ??? Should I just make the note el change on hover?
 
-// <span class="tooltip">This is my tooltip</span>
+	// <span class="tooltip">This is my tooltip</span>
 
-// const myDiv = document.getElementById("myDiv");
-// myDiv.addEventListener("mouseover", showTooltip);
-// myDiv.addEventListener("mouseout", hideTooltip);
+	// const myDiv = document.getElementById("myDiv");
+	// myDiv.addEventListener("mouseover", showTooltip);
+	// myDiv.addEventListener("mouseout", hideTooltip);
 
-// function showTooltip() {
-//    const tooltip = document.querySelector(".tooltip");
-//    tooltip.style.display = "block";
-// }
-// function hideTooltip() {
-// 	//get through "target?"
-//    const tooltip = document.querySelector(".tooltip");
-//    tooltip.style.display = "none";
-// }
+	// function showTooltip() {
+	//    const tooltip = document.querySelector(".tooltip");
+	//    tooltip.style.display = "block";
+	// }
+	// function hideTooltip() {
+	// 	//get through "target?"
+	//    const tooltip = document.querySelector(".tooltip");
+	//    tooltip.style.display = "none";
+	// }
 
-// .tooltip {
-//    display: none;
-//    background-color: yellow;
-//    color: black;
-// }
+	// .tooltip {
+	//    display: none;
+	//    background-color: yellow;
+	//    color: black;
+	// }
 
 
-// get all note div's
-	// lead-notes-1 > div > table
-	// foreach
-		//skip first
-		// get second tr > td of tbody
-		// get element's innerHTML
-		// make it the content of a tooltip (template?)
-		// add mouseover fn to tr to show tooltip
-		// add mouseout fn to tr to hide tooltip
+	// get all note div's
+		// lead-notes-1 > div > table
+		// foreach
+			//skip first
+			// get second tr > td of tbody
+			// get element's innerHTML
+			// make it the content of a tooltip (template?)
+			// add mouseover fn to tr to show tooltip
+			// add mouseout fn to tr to hide tooltip
 
 
 /*** PLAN INFO SELECT ***/
-/*  Function getMAPip
+
+/* Function getT2MAPip
 	gets the Medicare Advantage PIP, if there is one to get. */
-function getMAPip() {
+function getT2MAPip() {
 	// TODO: check for just one page...or a page w/o a Medicare Advantage
 
 	var maPip = $$('#appInfoContainer div:contains("Medicare Advantage")');
@@ -111,7 +183,21 @@ function getMAPip() {
 	return maPip[0];
 }
 
-/*  Function getMostRecentDtcPip
+/* Function getT3MAPip
+	gets the Medicare Advantage PIP, if there is one to get. */
+function getT3MAPip() {
+	// TODO: check for just one page...or a page w/o a Medicare Advantage
+
+	var maPip = $$('#appInfoContainer div:contains("Medicare Advantage")');
+	if(maPip[0] == undefined) {
+		console.warn("Could not find PIP");
+		return undefined;
+	}
+
+	return maPip[0];
+}
+
+/* Function getMostRecentDtcPip
 	gets the DTC PIP, if there is one to get. */
 function getMostRecentDtcPip() {
 	// TODO: check for just one page...or a page w/o a DTC
@@ -121,7 +207,7 @@ function getMostRecentDtcPip() {
 	return dtcPip[0];
 }
 
-/*  Function getT2AgentName
+/* Function getT2AgentName
 	gets the T2 agent Name, if there is one to get. */
 function getT2AgentName() {
 	// TODO: check for just one page...or a page w/o a DTC
@@ -136,13 +222,13 @@ function getT2AgentName() {
 	return t2AgentData.replace(/(.*) \(\d+\)/,"$1");
 }
 
-/*  Function getPlanData
+/* Function getPlanData
 	gets the plan's name and code, if there is one to get. */
 function getPlanData() {
 	// TODO: check for just one page...or a page w/o a Medicare Advantage
 
 	//get the Medicare Advantage page
-	var MAPip = getMAPip();
+	var MAPip = getT2MAPip();
 	if (MAPip.length == 0) 
 		return "";
 
@@ -152,13 +238,13 @@ function getPlanData() {
 	return planName + " " + planId;
 }
 
-/*  Function getSep
+/* Function getSep
 	gets the SEP, if there is one to get. */
 function getSep() {
 	// TODO: check for just one page...or a page w/o a Medicare Advantage
 
 	//get the Medicare Advantage page
-	var MAPip = getMAPip();
+	var MAPip = getT2MAPip();
 	if (MAPip.length == 0) 
 		return "";
 
@@ -167,13 +253,13 @@ function getSep() {
 	return subDate.replace(/(\s+)(.*)(\s+)/,"$2");
 }
 
-/*  Function getSubDate
+/* Function getSubDate
 	gets the T2 agent Name, if there is one to get. */
 function getSubDate() {
 	// TODO: check for just one page...or a page w/o a Medicare Advantage
 
 	//get the Medicare Advantage page
-	var MAPip = getMAPip();
+	var MAPip = getT2MAPip();
 	if (MAPip.length == 0) 
 		return "";
 
@@ -182,8 +268,33 @@ function getSubDate() {
 	return subDate.replace(/([\d\/]+) (.*)/,"$1");
 }
 
-/*  Function selectAppInfo
-	Event function that selects and copies the correct node containing the cu's processed and formatted info */
+/* Function getPlanStartDate
+	gets the T2 agent Name, if there is one to get. */
+function getPlanStartDate() {
+	// TODO: check for just one page...or a page w/o a Medicare Advantage
+
+	//get the Medicare Advantage page
+	var MAPip = getT2MAPip();
+	if (MAPip.length == 0) 
+		return "";
+
+	var subDate = MAPip.querySelector('div[data-testid="date-created"]').innerHTML;
+	
+	return subDate.replace(/([\d\/]+) (.*)/,"$1");
+}
+
+/* Function selectAppInfo
+	Event function that selects and copies the correct node containing the cu's processed and formatted info 
+
+	OUTPUT:
+		T2 Agent:
+		T3 Agent:
+		Plan:
+		SEP:
+		Sub Date:
+		Eff Date:
+		Alt Address:
+	*/
 function selectAppInfo(evt) {
 	// CTRL + SHIFT + X
 	if (evt.ctrlKey && evt.shiftKey && evt.which == 88) {
@@ -193,9 +304,12 @@ function selectAppInfo(evt) {
 
 		var finalString = 
 			"T2 Agent:\t"+getT2AgentName()+
+			"T3 Agent:\t"+getT3AgentName()+
 			"\nPlan:\t"+getPlanData()+
 			"\nSEP:\t"+getSep()+
 			"\nSub Date:\t"+getSubDate();
+			"\nEff Date:\t"+getSubDate();
+			"\nAlt Address:\t"+getSubDate();
 
 		navigator.clipboard.writeText(finalString).then(() => {
 		  console.log('Content copied to clipboard');
@@ -214,8 +328,19 @@ function selectAppInfo(evt) {
 document.bodebug = false; // TODO: Make this survive minif.
 if(document.ranSetup != true) {
 	setUpKeyboardShortcuts();
+
 	document.ranSetup = true;
+	document.unloadBO = unloadBO;
 	document.alreadyPresent = alreadyPresent;
+	evt = { // For debugging/testing
+		ctrlKey:true,
+		shiftKey:true,
+		which:70
+	}
 } else {
 	document.alreadyPresent();
 }
+
+
+
+
