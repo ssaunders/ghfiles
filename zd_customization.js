60f9d27c62ee44e4c341813b63ca7702b3e7988c
ssaunders <stersaun@gmail.com>
2024-04-18 16:18:55 -0600

    Update zd_customization.js
    Modified FOCUS SUBJECT LINE to not put something down if the -ST is already there

--- zd_customization.js
+++ zd_customization.js
@@ -3,22 +3,25 @@
 // TODO: 
 
 // working on vvv
+// TODO: Make a "copy Lead ID" shortcut C+S+L
+// TODO: Make a "copy MBI" shortcut C+S+B
 
 // priority vvv
-// TODO: Fix DNE copy to handle Devoted RFI's
-// TODO: Make Ctrl+Shift+E also work for Dev RFI'S
 
+// TODO: Make a "CSNP setup" shortcut: "member", date, fill out AOR info
 // TODO: Make the "get first comment" and "process comment" into a library fn
 // TODO: Make a shortcut to scroll to the most recent Cu Info
 // TODO: Make view refresher that sends me a notification if certain views have stuff available.
 // TODO: make shortcut to focus on internal note 
-// TODO: Make a subject sorter...for just the page.
+// TODO: Make a subject sorter...for just the view page.
 	// (needs to ignore !)
 	// TODO: Finish adding ZD object
 		// TODO: Get it unset ZD
 	// (sort groups of things seperately)
 
 /** DONE **/
+	// TODO: Make C+S+S not put something down if the -ST is already there
+	// TODO: Make Ctrl+Shift+E also work for Dev RFI'S
 	// TODO: Make a " - XX" addition for state for Subj C+S+T
 	// TODO: Make Ctrl+Shift+S also focus search bar
 	// TODO: TEST to see if HM email copy fn actually filters out correct plans
@@ -35,8 +38,11 @@
 	// TODO: Make an unload fn
 	// TODO: Make document hold ref's to all these things, so can test/call/replace
 
-/* Function DEFAULT
-   NOTES_ON_FN */
+/* Function function_name
+	NOTES_ON_FN */
+function function_name(argument) {
+	// body...
+}
 
 /** Random notes:
  {{ticket.requester.customfields.state}} 
@@ -46,8 +52,8 @@
 
 
 /*************
- * FUNCTIONS
- *************/
+* FUNCTIONS
+**************/
 
 /*** LIBRARY ***/
 
@@ -95,11 +101,11 @@
 			return;
 		}
 
-	    var range = document.createRange();
-	    var sel = document.getSelection();
+		 var range = document.createRange();
+		 var sel = document.getSelection();
 
-	    sel.removeAllRanges();
-	    range.selectNodeContents(htmlEl);
+		 sel.removeAllRanges();
+		 range.selectNodeContents(htmlEl);
 		sel.addRange(range);
 		document.execCommand("Copy");
 	}
@@ -144,7 +150,7 @@
 /*** UTILITY ***/
 
 	/* Function setUpKeyboardShortcuts
-	   Sets up the keyboard listeners to the page */
+		Sets up the keyboard listeners to the page */
 	function setUpKeyboardShortcuts() {
 		document.addEventListener("keyup", selectTicketInfo);
 		document.addEventListener("keyup", selectFirstComment);
@@ -156,7 +162,7 @@
 	}
 
 	/* Function unload
-	   Removes the keyboard listeners from the page */
+		Removes the keyboard listeners from the page */
 	function unload() {
 		document.removeEventListener("keyup", selectTicketInfo);
 		document.removeEventListener("keyup", selectFirstComment);
@@ -169,7 +175,7 @@
 	}
 
 	/* Function isCarrierPlan
-	   Copies "Not a HM Plan" to the clipboard */
+		Copies "Not a HM Plan" to the clipboard */
 	function isCarrierPlan(cuInfoEl,carrierName) {
 		if(carrierName == undefined || carrierName == null) return false;
 		var regExp = new RegExp("Plan Name:\\s+"+carrierName);
@@ -177,16 +183,17 @@
 	}
 
 	/* Function parseCuInfo
-	   Parses the cu info el text into an array */
+		Parses the string of cu info pulled from the el 
+		into an array */
 	function parseCuInfo(cuInfo) {
-		return cuInfo.innerHTML.replace(/ ?\&nbsp\;/g,"").split("<br>");
+		return cuInfo.replace(/ ?\&nbsp\;/g,"").split("<br>");
 	}
 
 
 /*** FOCUS INTERNAL NOTE ***/
 
 	/* Function getNoteEl
-	   Selects the correct node that contains the cu's processed and formatted info */
+		Selects the correct node that contains the cu's processed and formatted info */
 	function getNoteEl() {
 		// var firstInfoElList = $('div.zd-comment:contains("Agent Name:")');
 		// var firstInfoElListLn = firstInfoElList.length;
@@ -198,7 +205,7 @@
 	}
 
 	/* Function setFocusToNoteEl
-	   Event function that selects and copies the correct node containing the cu's processed and formatted info */
+		Event function that selects and copies the correct node containing the cu's processed and formatted info */
 	function setFocusToNoteEl(evt) {
 		// CTRL + SHIFT + ?
 		// if (evt.ctrlKey && evt.shiftKey && evt.which == 70) {
@@ -211,11 +218,11 @@
 		// 		return;
 		// 	}
 
-		//     var range = document.createRange();
-		//     var sel = document.getSelection();
+		//	  var range = document.createRange();
+		//	  var sel = document.getSelection();
 
-		//     sel.removeAllRanges();
-		//     range.selectNodeContents(firstInfoEl);
+		//	  sel.removeAllRanges();
+		//	  range.selectNodeContents(firstInfoEl);
 		// 	sel.addRange(range);
 		// 	document.execCommand("Copy");
 		// }
@@ -225,7 +232,7 @@
 /*** FOCUS SUBJECT LINE ***/
 
 	/* Function getSubjLineEl
-	   Gets the subject element */
+		Gets the subject element */
 	function getSubjLineEl() {
 		var firstInfoElList = $('.fr-focus [placeholder="Subject"]');
 		if (firstInfoElList.length == 0) {
@@ -236,7 +243,7 @@
 	}
 
 	/* Function subjectFocus
-	   Event function that selects the Subject line, and focuses the cursor at the start */
+		Event function that selects the Subject line, and focuses the cursor at the start */
 	function subjectFocus(evt) {
 		// CTRL + SHIFT + S //s for subject
 		if (evt.ctrlKey && evt.shiftKey && evt.which == 83) {
@@ -246,7 +253,7 @@
 				return;
 			}
 
-			if(subjectIsFocused()) {
+			if(subjectIsFocused() && !hasStateAbbr()) {
 				addStateAbbr();
 			} else {
 				subjLineEl.focus();
@@ -259,25 +266,25 @@
 	/**** ADD STATE ABBREVIATION ****/
 
 	/* Function subjectIsFocused
-	   Event function that selects the Subject line, and focuses the cursor at the start */
+		Event function that selects the Subject line, and focuses the cursor at the start */
 	function subjectIsFocused() {
 		return document.activeElement === getSubjLineEl();
 	}
 
 	/* Function addStateAbbr
-	   Event function that selects the Subject line, and focuses the cursor at the start */
+		Event function that selects the Subject line, and focuses the cursor at the start */
 	function addStateAbbr() {
 		var subjLineEl = getSubjLineEl(),
 			 ticketInfoEl = getTicketInfoEl(),
 			 stateAbbr;
 
-	   if(subjLineEl === undefined) {
-	   	console.warn(">> Could not add state abbr to subject: subject line not found");
-	   } else if (ticketInfoEl === undefined) {
-	   	console.warn(">> Could not add state abbr to subject: processed ticket info not found");
-	   }
+		if(subjLineEl === undefined) {
+			console.warn(">> Could not add state abbr to subject: subject line not found");
+		} else if (ticketInfoEl === undefined) {
+			console.warn(">> Could not add state abbr to subject: processed ticket info not found");
+		}
 
-	   //have to declare variables here, b/c minifier madness
+		//have to declare variables here, b/c minifier madness
 		var ticketInfo = ticketInfoEl.innerHTML.replaceAll("&nbsp;","");
 		var stringPos = ticketInfo.search(/, ([A-Z]{2}) \d{5}/);
 		// extra space to give something to backspace, so there 
@@ -288,11 +295,23 @@
 		subjLineEl.value += stateAbbr;
 	}
 
+	/* Function hasStateAbbr
+		Event function that selects the Subject line, and focuses the cursor at the start */
+	function hasStateAbbr() {
+		var subjLineEl = getSubjLineEl();
+
+		if(subjLineEl === undefined) {
+			console.warn(">> Could not add state abbr to subject: subject line not found");
+		}
+
+		return subjLineEl.value.search(/ \- [A-Z]{2}/) != -1;
+	}
+
 
 /*** FOCUS ACTIVE SEARCH ***/
 
 	/* Function getSearchBox
-	   Gets the search Box */
+		Gets the search Box */
 	function getSearchBox() {
 		var searchBox = $('.search [data-garden-id="forms.faux_input"] input')[0];
 		if (searchBox == undefined) {
@@ -303,7 +322,7 @@
 	}
 
 	/* Function searchBoxFocus
-	   Event fn that moves the focus to the active search box */
+		Event fn that moves the focus to the active search box */
 	function searchBoxFocus(evt) {
 		// CTRL + SHIFT + S //s for Search
 		if (evt.ctrlKey && evt.shiftKey && evt.which == 83) {
@@ -322,25 +341,25 @@
 /*** FIRST COMMENT SELECT ***/
 
 	/* Function isT3Feedback
-	   Decides if the el passed in is the first comment on a Dev RFI*/
+		Decides if the el passed in is the first comment on a Dev RFI*/
 	function isT3Feedback(el) {
 		return el.innerHTML.search(/Hi Case Management/) != -1;
 	}
 
 	/* Function isDevotedRFI
-	   Decides if the el passed in is the first comment on a Dev RFI*/
+		Decides if the el passed in is the first comment on a Dev RFI*/
 	function isDevotedRFI(el) {
 		return el.innerHTML.search(/<table>/) == 0;
 	}
 
 	/* Function isRegularRFI
-	   Decides if the el passed in is the first comment on a regular RFI */
+		Decides if the el passed in is the first comment on a regular RFI */
 	function isRegularRFI(el) {
 		return el.innerHTML.search(/Agent Name\:/) == 14;
 	}
 
 	/* Function getFirstCommentEl
-	   Selects the el that contains the cu's info, which is the first comment.
+		Selects the el that contains the cu's info, which is the first comment.
 		Returns null if not in a format recognized */
 	function getFirstCommentEl() {
 		var firstInfoEl = $('.fr-focus div.zd-comment')[0];
@@ -355,7 +374,7 @@
 	}
 
 	/* Function processDevotedRFIComment
-	   Processes the content of a Devoted RFI initial comment into the standardized format */
+		Processes the content of a Devoted RFI initial comment into the standardized format */
 	function processDevotedRFIComment(el) {
 		var rfiParts= el.innerHTML.replaceAll(/\<\/?(t|b)[rdba](ody|ble)?( rowspan=\"\d\")?\>/g,"!").split(/!+/g);
 
@@ -375,25 +394,25 @@
 
 
 	/* Function processT3FeedbackComment
-	   Processes the content of a Devoted RFI initial comment into the standardized format */
+		Processes the content of a Devoted RFI initial comment into the standardized format */
 	function processT3FeedbackComment(el) {
-		var commentParts= el.innerHTML.replaceAll(/\<\/?strong\>/g,"").replaceAll(/(\<\/?(p|br|u)\>)|[\n]|(: )/g,"!").split(/!+/g);
-
+		var commentParts= el.innerHTML.replaceAll(/:? ?<\/?(br|strong|u|p)>[\n]?(: )?( )?/g, "|").split(/\|+/g);
 		var returnVal = 
-		"Cu Phone #:\t"+commentParts[5]+"\n"+
+		"Transf Code:\t"+commentParts[7]+"\n"+
+		"Cu Phone #:\t"+commentParts[5].replaceAll(/\(\)\-/g)+"\n"+
 		"T3 Agent:\t"+commentParts[1].replace(/ has submitted.*/,"")+"\n"+
 		"T3 Team Mgr:\t"+commentParts[13]+"\n"+
 		"T3 Director:\t"+commentParts[15]+"\n"+
-		"T2 Agent:\t"+commentParts[3].replace(/ \d+/,"")+"\n"+
-		"T2 Team Mgr:\t"+"\n"+
-		"T2 Director:\t"+"\n"+
-		"Notes: \t"+commentParts[17];
+		"T2 Agent:\t"+commentParts[3].replace(/ \d+| One Digital| SHA/,"")+"\n"+
+		"T2 UID:\t"+commentParts[3].replace(/T2|[^\d]*/g,"")+"\n"+
+		"FB Action:\t"+commentParts[11]+"\n"+
+		"FB Notes: \t"+commentParts[17];
 
 		return returnVal;
 	}
 
 	/* Function selectFirstComment
-	   Event function that selects and copies the initial comment containing the RFI's info */
+		Event function that selects and copies the initial comment containing the RFI's info */
 	function selectFirstComment(evt) {
 		// CTRL + SHIFT + F //(f for "first")
 		if (evt.ctrlKey && evt.shiftKey && evt.which == 70) {
@@ -420,14 +439,14 @@
 /*** SELECT DNE INFO ***/
 
 	/* Function getCuName
-	   Get's the cu's name from the page */
+		Get's the cu's name from the page */
 	function getCuName() {
 		var cuName = $('.fr-focus [data-test-id=essentials-header-title]');
 	 	return cuName[0].innerHTML;
 	}
 
 	/* Function getLeadID
-	   Get's the lead id from the page */
+		Get's the lead id from the page */
 	function getLeadID() {
 		var searchAry = $('.fr-focus div:contains("@gohealth.com")');
 		var leadID = searchAry[searchAry.length-1].innerHTML;
@@ -436,7 +455,7 @@
 	}
 
 	/* Function selectDNEInfo
-	   Get's the info needed for a DNE from the page.
+		Get's the info needed for a DNE from the page.
 		Also formats it into a tabbed format and copies for pasting into a spreadsheet */
 	function selectDNEInfo(evt) {
 		// CTRL + SHIFT + E // (e for "engage")
@@ -445,12 +464,18 @@
 				console.warn(">> debug on");
 			}
 
+			var commentEl = getFirstCommentEl(),
+				 agentNameData = commentEl.children[0].innerHTML;
+			if(isDevotedRFI(commentEl)) {
+				agentNameData = agentNameData.replaceAll(/\<\/?(t|b)[rdba](ody|ble)?( rowspan=\"\d\")?\>/g,"!").split(/!+/g)[1];
+			}
+
 			var finalString = 
 				getCuName()+"\t"+
 				getLeadID()+"\t"+
-				getFirstCommentEl().children[0].innerHTML;
+				agentNameData;
 
-		   // TODO: get inner HTML, and if Dev, get agent Name
+			// TODO: get inner HTML, and if Dev, get agent Name
 
 
 			copyStringToClipboard(finalString);
@@ -463,11 +488,11 @@
 /*** SELECT HM EMAIL INFO ***/
 
 	/* Function selectHumEmailInfo
-	   Copies cu info and formats it to the HM Email format
+		Copies cu info and formats it to the HM Email format
 
 		OUTPUT:
 		Reason	Lead ID	Sub_Date	Agent_SAN	Client_Name	DOB	ZIP	MBI
-	   */
+		*/
 	function selectHumEmailInfo(evt) {
 		// CTRL + SHIFT + H // (h for "Humana")
 		if (evt.ctrlKey && evt.shiftKey && evt.which == 72) {
@@ -518,7 +543,7 @@
 /*** CU INFO SELECT ***/
 
 	/* Function getTicketInfoEl
-	   Gets the most recent comment with the app's processed and formatted info */
+		Gets the most recent comment with the app's processed and formatted info */
 	function getTicketInfoEl() {
 		var cuInfoElList = $('.fr-focus div.zd-comment:contains("Cu/Agent Info")'),
 			 cuInfoElListLn = cuInfoElList.length,
@@ -538,7 +563,7 @@
 	}
 
 	/* Function selectTicketInfo
-	   Event function that copies the most recent comment with the app's processed and formatted info */
+		Event function that copies the most recent comment with the app's processed and formatted info */
 	function selectTicketInfo(evt) {
 		// CTRL + SHIFT + X  //x b/c convenient
 		if (evt.ctrlKey && evt.shiftKey && evt.which == 88) {
@@ -552,8 +577,8 @@
 
 
 /*************
- * LOGIC
- *************/
+* LOGIC
+**************/
 if(typeof zd == "undefined") {
 	window.zd = {
 		ranSetup: false
@@ -566,9 +591,9 @@ if(zd.ranSetup != true) {
 	zd.unload = unload;
 	zd.alreadyPresent = alreadyPresent;
 	evt = { // For debugging/testing
-	    ctrlKey:true,
-	    shiftKey:true,
-	    which:70
+		 ctrlKey:true,
+		 shiftKey:true,
+		 which:70
 	}
 	zd.mydebug = mydebug;
 } else {
